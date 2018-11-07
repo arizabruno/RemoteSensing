1ee48131dbbd45b7edb0b9b8c78da53c90e2fb2e
Bruno Ariza <brunoariza@outlook.com>
2018-11-07 02:29:25 -0500

    probelema com a permisao quase resolvido, agora eh necessario ficar o bug de quando o app comeca sem estar com a permissao

--- app/src/main/java/com/example/bruno/wt/MainActivity.java
+++ app/src/main/java/com/example/bruno/wt/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bruno.wt;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -14,6 +15,10 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -33,6 +38,7 @@ import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.io.IOException;
+import java.security.Permission;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
     // Declaring the TextViews used in the layout
     TextView temperatureTextView, windForceTextView, locationTextView;
 
-    private final int FINE_LOCATION_PERMISSION = 9999;
+    private final int COARSE_LOCATION_PERMISSION = 9999;
 
     private static final String OPEN_WEATHER_URL = "https://api.darksky.net/forecast/e383ecf5828fc78c327e8d8cff4be13e/27.964157,-82.452606";
 
@@ -48,12 +54,34 @@ public class MainActivity extends AppCompatActivity {
 
     private double latitude, longitude;
 
+    private String localizacaoFinal = "teste";
+
+
     String darkSkyUrl;
 
     private boolean verificacaoConcluida = true;
 
     List<Address> localizacao;
 
+    private CheckBox checkBoxLocation;
+
+    private Task<Location> taskLocation;
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case COARSE_LOCATION_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager
+                        .PERMISSION_GRANTED) {
+                    DadosAsyncTask task = new DadosAsyncTask();
+                    task.execute(darkSkyUrl, localizacaoFinal);
+                }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -68,31 +96,17 @@ public class MainActivity extends AppCompatActivity {
         windForceTextView = (TextView) findViewById(R.id.wind_force_text_view);
         locationTextView = (TextView) findViewById(R.id.location_text_view);
 
+        Button ativarLocalizacao = (Button) findViewById(R.id.ativar_localizacao);
+
+
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
 
-
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission
-                .ACCESS_FINE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager
                 .PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
 
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission
-                    .ACCESS_FINE_LOCATION}, FINE_LOCATION_PERMISSION);
-
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission
-                    .ACCESS_COARSE_LOCATION}, FINE_LOCATION_PERMISSION);
-
-
+            ativarLocalizacao.isPressed();
 
 
 
@@ -100,48 +114,56 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        while (!checkingPermission()) {
-            checkingPermission();
-        }
+        ativarLocalizacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest
+                        .permission
+                        .ACCESS_COARSE_LOCATION}, COARSE_LOCATION_PERMISSION);
+
+            }
+        });
+
 
 
 
         darkSkyUrl = "https://api.darksky" +
                 ".net/forecast/e383ecf5828fc78c327e8d8cff4be13e/27.964157,-82.452606";
 
-        Task<Location> taskLocation = mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            // Logic to handle location object
-
-                        }
-
-                        latitude = location.getLatitude();
-                        longitude = location.getLongitude();
+        taskLocation = mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(MainActivity.this, new
+                        OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                // Got last known location. In some rare situations this can be null.
+                                if (location != null) {
+                                    // Logic to handle location object
+                                    latitude = location.getLatitude();
+                                    longitude = location.getLongitude();
 
 
-                        try {
-                            localizacao = geocoder.getFromLocation(latitude, longitude,
-                                    1);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                                    try {
+                                        localizacao = geocoder.getFromLocation(latitude, longitude,
+                                                1);
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
 
-                        String locl = localizacao.get(0).getLocality() + ", " + localizacao.get(0)
-                                .getAdminArea();
+                                    localizacaoFinal = localizacao.get(0).getLocality() + ", " + localizacao.get(0)
+                                            .getAdminArea();
 
-                        darkSkyUrl = "https://api.darksky.net/forecast/e383ecf5828fc78c327e8d8cff4be13e/" +
-                                latitude + "," + longitude;
-
-                        DadosAsyncTask task = new DadosAsyncTask();
-                        task.execute(darkSkyUrl, locl);
-                    }
+                                    darkSkyUrl = "https://api.darksky.net/forecast/e383ecf5828fc78c327e8d8cff4be13e/" +
+                                            latitude + "," + longitude;
+                                }
 
 
-                });
+
+
+                            }
+
+
+                        });
 
 
 
@@ -155,6 +177,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             Dados finalDados = QueryUtils.fetchDados(urls[0]);
+
             finalDados.setLocation(urls[1]);
 
             return finalDados;
@@ -184,9 +207,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public boolean checkingPermission() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission
-                .ACCESS_FINE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+        if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager
                 .PERMISSION_GRANTED) {
             // TODO: Consider calling
